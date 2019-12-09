8156f42b12431bb9199e96d09cfda9b16d8494ca
PapyrusCompendium <51677630+PapyrusCompendium@users.noreply.github.com>
2019-12-09 13:41:49 -0500

    Update and rename ActiveDirectoryUtilities.ps1 to ADUtilities.ps1

--- ADUtilities.ps1
+++ ADUtilities.ps1
@@ -18,7 +18,7 @@ $script:banner = "
 
 #Author: PapyrusCompendium
 #Description: Help with quick utility functions in Active Directory. Mainly this is for my own use.
-
+Import-Module "$(Get-Location)\ADUtiliesModule.ps1"
 $script:foundADObject
 $script:alternateCreds
 
@@ -66,13 +66,14 @@ function DisplayUserData{
 function DisplayComputerData{
     "  ┌────────────────────────┐       ┌───────────────────────Requested Data──────────────────────"
     "  │------Display Data------│       │"
-    "  │IPv4 Address:          0│       │"
+    "  │Get Last User:         0│       │"
     "  │Team Viewer  ID:       1│       │"
     "  │Dell Service Tag       2│       │"
     "  │Installed Software:    3│       │"
     "  │Custom Query:          4│       │"
     "  │All Data:              *│       │"
     "  ├────────────────────────┤       │"
+    "  │! Restart Machine      R│       │"
     "  │< Go Back              #│       │"
     "  └────────────────────────┘"
 }
@@ -145,169 +146,6 @@ function WriteTextInSidePanel{param($textData)
     SetCursorLocation 0 36
 }
 
-function SelectADUser { param ($adUsers)
-    ClearBanner
-
-    if($adUsers.GetType().Name -ne "ADUser"){
-        for($x = 0; $x -lt $adUsers.Count; $x++){
-            "   $($adUsers[$x].Name)[$($x)]"
-        }
-        "   Go Back[-1]"
-    
-        [int16]$index = Read-Host -Prompt "   Option"
-        if($index -eq -1){
-            return
-        }
-    
-        $script:foundADObject =  $adUsers[$index]
-    } else {
-        $script:foundADObject = $adUsers
-    }
-
-    UserDataMenu
-}
-
-function SelectADComputer { param ($adComputers)
-    ClearBanner
-    if($adComputers.GetType().Name -ne "ADComputer"){
-        for($x = 0; $x -lt $adComputers.Count; $x++){
-            "   $($adComputers[$x].Name)[$($x)]"
-        }
-        "   Go Back[-1]"
-    
-        [int16]$index = Read-Host -Prompt "   Option"
-        if($index -eq -1){
-            return
-        }
-    
-        $script:foundADObject =  $adComputers[$index]
-    } else {
-        $script:foundADObject = $adComputers
-    }
-
-    ComputerDataMenu
-}
-
-function ChangeEmployeeID{
-    $employeeID = Read-Host -Prompt "New Employee ID"
-    Set-ADUser $script:foundADObject -EmployeeID  $employeeID
-}
-
-function ChangeUserPassword{
-    $newPassword = Read-Host -Prompt "New Password" -AsSecureString
-    Set-ADAccountPassword $script:foundADObject -Reset -NewPassword $newPassword
-}
-
-function SearchByName{
-    ClearBanner
-    $name = "*$(Read-Host -Prompt "Name")*".Replace(" ", "*")
-
-    if(Get-ADUser -F {Name -like $name}){
-        SelectADUser(Get-ADUser -F {Name -like $name} -Properties *)
-    } else {
-        "No employees found!"
-        Pause
-    }
-}
-
-function SearchByUsername{
-    ClearBanner
-    $username = "*$(Read-Host -Prompt "Username")*"
-
-    if(Get-ADUser -F {SamAccountName -like $username}){
-        SelectADUser(Get-ADUser -F {SamAccountName -like $username} -Properties *)
-    } else {
-        "Employee not found!"
-        Pause
-    }
-}
-
-function SearchByEmployeeID{
-    ClearBanner
-    $employeeID = Read-Host -Prompt "Employee ID"
-
-    if(Get-ADUser -F {EmployeeID -eq $employeeID}){
-        SelectADUser(Get-ADUser -F {EmployeeID -eq $employeeID} -Properties *)
-    } else {
-        "Employee not found!"
-        Pause
-    }
-}
-
-function SearchByComputerName {
-    ClearBanner
-    $computerName = "*$(Read-Host -Prompt "Computer Name")*".Replace(" ", "*")
-    
-    if(Get-ADComputer -F {Name -like $computerName}){
-        SelectADComputer(Get-ADComputer -F {Name -like $computerName} -Properties *)
-    } else {
-        "Computer not found!"
-        Pause
-    }
-}
-
-function SearchByIP {
-    ClearBanner
-    $deviceIP = Read-Host -Prompt "Computer IP"
-    
-    if(Get-ADComputer -F {IPv4Address -eq $deviceIP} -Properties *){
-        SelectADComputer(Get-ADComputer -F {IPv4Address -eq $deviceIP} -Properties *)
-    } else {
-        "Computer not found!"
-        Pause
-    }
-}
-
-function AlternateCredential {
-    ClearBanner
-    $username = Read-Host -Prompt "Username"
-    $password = Read-Host -Prompt "Password" -AsSecureString
-    $script:alternateCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
-}
-
-function GetTeamViewerID {param($computerName)
-    $wmiObject
-    if(!$script:alternateCreds)
-    {
-        $wmiObject = Get-WmiObject -List StdRegProv -ComputerName $computerName
-    } else {
-        $wmiObject = Get-WmiObject -List StdRegProv -ComputerName $computerName -Credential $script:alternateCreds
-    }
-
-    return $wmiObject.GetDWORDValue(2147483650, "SOFTWARE\WOW6432Node\TeamViewer", "ClientID").uValue
-}
-
-function GetDellServiceTag {param($computerName)
-    if(!$script:alternateCreds)
-    {
-        return (Get-WmiObject -Class win32_bios -ComputerName $computerName).SerialNumber
-    }
-
-    return (Get-WmiObject -Class win32_bios -ComputerName $computerName -Credential $script:alternateCreds).SerialNumber
-}
-
-function GetInstalledSoftware {param($computerName)
-    $softwareList = @()
-    if(!$script:alternateCreds)
-    {
-        $wmiObject = Get-WmiObject -List StdRegProv -ComputerName $computerName -Credential $adCreds
-    } else {
-        $wmiObject = Get-WmiObject -List StdRegProv -ComputerName $computerName
-    }
-
-    foreach ($item in $wmiObject.EnumKey(2147483650, "SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall").sNames |
-    ? {($wmiObject.GetDWORDValue(2147483650, "SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$($_)", "SystemComponent").uValue -ne 1)}) {
-        $softwareList += $wmiObject.GetStringValue(2147483650, "SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$($item)", "DisplayName").sValue
-    }
-
-    foreach ($item in $wmiObject.EnumKey(2147483650, "SOFTWARE\Wow6432node\Microsoft\Windows\CurrentVersion\Uninstall").sNames |
-    ? {($wmiObject.GetDWORDValue(2147483650, "SOFTWARE\Wow6432node\Microsoft\Windows\CurrentVersion\Uninstall\$($_)", "SystemComponent").uValue -ne 1)}) {
-        $softwareList += $wmiObject.GetStringValue(2147483650, "SOFTWARE\Wow6432node\Microsoft\Windows\CurrentVersion\Uninstall\$($item)", "DisplayName").sValue
-    }
-
-    return $softwareList | Where-Object {$_ -notlike "*Update for*"} | Sort-Object
-}
-
 function MainMenu{
     ClearBanner
     HelpDisplay
@@ -372,10 +210,17 @@ function ComputerDataMenu
         switch($optionSelection)
         {
             "#" {return}
+            "r" {RestartMachine($script:foundADObject.Name)}
             "*" {WriteInKeyPairSidePanel($script:foundADObject.GetEnumerator() | Where-Object {$_.Key -like "*"})}
-            "0" {WriteInKeyPairSidePanel($script:foundADObject.GetEnumerator() | Where-Object {$_.Key -eq "IPv4Address"})}
+            "0" {WriteListInSidePanel(GetLastUser($script:foundADObject.Name))}
             "1" {WriteTextInSidePanel("TeamViewer ID: $(GetTeamViewerID($script:foundADObject.Name))")}
-            "2" {WriteTextInSidePanel("Dell Service Tag: $(GetDellServiceTag($script:foundADObject.Name))")}
+            "2" {
+                $serviceTag = GetDellServiceTag($script:foundADObject.Name)
+                $machineData = @()
+                $machineData += GetMachineType($serviceTag)
+                $machineData += "Dell Service Tag: $($serviceTag)"
+                WriteListInSidePanel($machineData)
+            }
             "3" {WriteListInSidePanel($(GetInstalledSoftware($script:foundADObject.Name)))}
             "4" {
                 $query = Read-Host -Prompt "Query"
